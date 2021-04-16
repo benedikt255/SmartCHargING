6096a330d8e8fcd2f825d6539e962b853a1f9516
Benedikt255 <Benedikt.Geib@googlemail.com>
2021-04-16 23:39:13 +0200

    print prices

--- smartLoadING.py
+++ smartLoadING.py
@@ -1,5 +1,18 @@
 import requests
+import time
+import datetime
+import matplotlib.pyplot as plt
 
 r = requests.get('https://api.awattar.de/v1/marketdata')
 data = r.json()['data']
-print(data)
+hours = []
+prices = []
+for point in data :
+    prices.append(point['marketprice'])
+    print(point['marketprice'])
+    hours.append(datetime.datetime.fromtimestamp(point['start_timestamp']/1000).strftime('%H'))
+    print(datetime.datetime.fromtimestamp(point['start_timestamp']/1000))
+print(time.time())
+print(int(time.time()*1e6))
+plt.plot(hours, prices)
+plt.show()
