980150b9254f28f21a218fafcd41aa84607e2f97
Benedikt255 <Benedikt.Geib@googlemail.com>
2021-04-17 09:48:03 +0200

    + flexible start/end time

--- smartLoadING.py
+++ smartLoadING.py
@@ -3,8 +3,10 @@ import time
 import datetime
 import matplotlib.pyplot as plt
 
-startTime = int(time.time()*1e3)
-endTime = int((time.time()+ 24*3600)*1e3)
+hoursBeforeNow = 24
+hoursFromNow = 24
+startTime = int((time.time()- hoursBeforeNow*3600)*1e3)
+endTime = int((time.time()+ hoursFromNow*3600)*1e3)
 r = requests.get('https://api.awattar.de/v1/marketdata?start='+str(startTime)+'&end='+str(endTime))
 data = r.json()['data']
 hours = []
@@ -12,10 +14,10 @@ prices = []
 for point in data :
     prices.append(point['marketprice']/1000+0.17)
     print(point['marketprice'])
-    hours.append(datetime.datetime.fromtimestamp(point['start_timestamp']/1000).strftime('%H'))
+    hours.append(datetime.datetime.fromtimestamp(point['start_timestamp']/1000))
     print(datetime.datetime.fromtimestamp(point['start_timestamp']/1000))
 print(time.time())
-print(int(time.time()*1e6))
+print(int(time.time()*1e3))
 plt.xlabel('hours')
 plt.ylabel('price €/kWh')
 plt.plot(hours, prices)
